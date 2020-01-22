25fad90d690897cdec3a64ccd525d30e7d8be761
TheWanderingMinstrel <dfmcla@gmail.com>
2020-01-22 12:25:11 -0800

    add ExecSQLList API:
                sql = "select * from a; select * from b; select * from c;";
                var s = SQLStatement.ExecSQLList(sql);

--- psql/Program.cs
+++ psql/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using adb;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Diagnostics;
+using System.IO;
 
 using adb.physic;
 using adb.utils;
@@ -25,16 +28,57 @@ namespace psql
 
         static void Main(string[] args)
         {
-            string sql = "select * from a where a1>1;select * from a where a1>1;select * from a where a1>1;select * from a where a1>1;select * from a where a1>1;";
-            Console.WriteLine(sql);
-            var results = SQLStatement.ExecSQL(sql, out string phyplan, out _);
-            Console.WriteLine(phyplan);
+            string path = @"C:\Users\dmclaugh\Desktop\Archive\tmp\Test.txt";
+            //string sql = "select * from a where a1>1;select * from a where a1>1;select * from a where a1>1;select * from a where a1>1;select * from a where a1>1;";
+            string sql = "select a2 from a where a.a3 > (select min(b1*2) from b where b.b2 >= (select c2-1 from c where c.c2=b2) and b.b3 > ((select c2 from c where c.c2=b2)));";
 
-            sql = "select * from d where d1=1;";
-            Console.WriteLine(sql);
-            results = SQLStatement.ExecSQL(sql, out phyplan, out _);
-            Console.WriteLine(phyplan);
 
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+
+                var results = SQLStatement.ExecSQL(sql, out string phyplan, out _);
+
+             
+                Console.WriteLine("******************************************")
+                Console.WriteLine(phyplan);
+
+                sw.WriteLine(sql);
+                sw.WriteLine(phyplan);
+            }
+                    Console.WriteLine("\n\n\n Begin Test\n\n");
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while ((sql = sr.ReadLine()) != null)
+                {
+                    string answer = sr.ReadLine();
+
+                    Console.WriteLine(sql);
+                    Console.WriteLine(answer);
+                    Console.WriteLine("\n\n\n");
+
+
+                    var results = SQLStatement.ExecSQL(sql, out string phyplan, out _);
+
+                    bool equal = String.Equals(answer, phyplan, StringComparison.InvariantCulture);
+                    Console.WriteLine($"The two plans {(equal == true ? "are" : "are not")} the same.");
+
+
+                    //sql = "select * from d where d1=1;";
+
+                    //results = SQLStatement.ExecSQL(sql, out phyplan, out _);
+                    //string banner = "*****Here Comes the Plan * ****";
+                    //Console.WriteLine(banner);
+                    //Console.WriteLine(sql);
+                    //Console.WriteLine(phyplan);
+
+
+                    //sw.WriteLine(sql);
+                    //sw.WriteLine(phyplan);
+
+
+                }
+            }
         }
     }
 }
