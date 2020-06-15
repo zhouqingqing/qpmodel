a657bfa854b67cd93f6ea3fce8195e6a9e000357
zhouqingqing <zhouqq.github@gmail.com>
2020-06-15 14:29:59 -0700

    create a server project to model certain aspects of server integration

---
+++ server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
