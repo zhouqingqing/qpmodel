153f93885d93ab7a82c67c04b969673b235fe506
arzuschen <xianghao.chen@gmail.com>
2020-07-07 14:06:22 -0700

    remove training empty line

--- qpmodel/optimizer.cs
+++ qpmodel/optimizer.cs
@@ -545,7 +545,6 @@ namespace qpmodel.optimizer
                     var childgroup = (child as PhysicMemoRef).Group();
                     childgroup.CalculateMinInclusiveCostMember(subproperty);
                     cost += childgroup.propertyOptimum_[subproperty].Value;
-
                 }
                 if (cost < propmincost)
                 {
