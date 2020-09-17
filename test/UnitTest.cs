f7ad1e640d0f2f7793ac3c5501f833d84b6387c4
9DemonFox <1196752479@qq.com>
2020-09-17 09:28:41 +0800

    delete redundant test case

--- test/UnitTest.cs
+++ test/UnitTest.cs
@@ -1138,41 +1138,7 @@ namespace qpmodel.unittest
                 sql = "select a1 from a where a2 > (select b1 from b where b3>=a3);";
                 var result = TU.ExecuteSQL(sql, out phyplan, option); Assert.IsTrue(TU.error_.Contains("one row"));
             }
-        }
-
-        [TestMethod]
-        public void TestSubqueryInclusiveCost()
-        {
-            var files = Directory.GetFiles(@"../../../../tpch", "*.sql");
-            string scale = "0001";
-
-            Tpch.CreateTables(true);
-            Tpch.LoadTables(scale);
-
-            Tpch.AnalyzeTables();
-
-            // run tests and compare plan
-            string sql_dir_fn = "../../../../tpch";
-            string write_dir_fn = $"../../../../test/regress/output/tpch{scale}_d";
-            string expect_dir_fn = $"../../../../test/regress/expect/tpch{scale}_d";
-
-            ExplainOption.show_tablename_ = false;
-            // because there is a filter subquery in q20
-            var badQueries = new string[] { "q01", "q02", "q03", "q04", "q05",
-                                            "q06", "q07", "q08", "q09", "q10",
-                                            "q11", "q12", "q13", "q14", "q15",
-                                            "q16", "q17", "q18","q16", "q17", 
-                                            "q18","q19", "q21", "q22" };
-            try
-            {
-                ExplainOption.show_tablename_ = false;
-                UBenchmarks.RunFolderAndVerify(sql_dir_fn, write_dir_fn, expect_dir_fn, badQueries);
-            }
-            finally
-            {
-                ExplainOption.show_tablename_ = true;
-            }
-        }
+        } 
     }
 
     [TestClass]
